14f9c47831f523c669b84752f38e144d6b9e7473
Gianfranco Demarco <gianfranco@hlpy.co>
2022-12-28 16:24:43 +0100

    update job and more linting

--- src/visualization/image_visualizer.py
+++ src/visualization/image_visualizer.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from shapely.geometry import Polygon
 
+
 def show_image(
     image,
     bbox: Polygon = None,
     alpha: float = 0.2
-):    
+):
     if bbox:
-        int_coords = lambda x: np.array(x).round().astype(np.int32)
+
+        def int_coords(coords):
+            return np.array(coords).round().astype(np.int32)
+
         exterior = [int_coords(bbox.exterior.coords)]
         overlay = image.copy()
         cv2.fillPoly(overlay, exterior, color=(255, 0, 0))
         cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0, image)
-        
+
     return plt.imshow(image)

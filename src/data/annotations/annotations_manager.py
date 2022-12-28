530b43b442d658b2600ecfe36eeacd2f11c765c9
Gianfranco Demarco <gianfranco@hlpy.co>
2022-12-28 16:15:07 +0100

    some linting

--- src/data/annotations/annotations_manager.py
+++ src/data/annotations/annotations_manager.py
@@ -1,14 +1,15 @@
 import json
 
+
 class AnnotationsManager:
     def __init__(
         self,
-        annotations_path: str    
+        annotations_path: str
     ) -> None:
         self.annotations_path = annotations_path
-    
-        with open(self.annotations_path, 'r') as f:
-             self.annotations = json.loads(f.read())
+
+        with open(self.annotations_path, mode='r', encoding='utf-8') as file:
+            self.annotations = json.loads(file.read())
 
     def get_annotations(self):
         return self.annotations
--- src/data/annotations/coco_annotations_manager.py
+++ src/data/annotations/coco_annotations_manager.py
@@ -7,17 +7,17 @@ class CocoAnnotationsManager(AnnotationsManager):
 
     def __get_image_id__(self, image_path):
         return list(filter(lambda image: image['file_name'].endswith(image_path), self.annotations['images']))[0]['id']
-    
+
     def __get_image_annotation__(self, image_id):
         return list(filter(lambda annotation: annotation['image_id'] == image_id, self.annotations['annotations']))[0]
-    
+
     def get_annotation(self, image_path):
         """
         Image path is relative to data/
         """
 
         image_id = self.__get_image_id__(image_path)
-        annotation =  self.__get_image_annotation__(image_id)
+        annotation = self.__get_image_annotation__(image_id)
         return annotation
 
     def get_bbox(self, image_path):
@@ -27,19 +27,21 @@ class CocoAnnotationsManager(AnnotationsManager):
 
         annotation = self.get_annotation(image_path=image_path)
         return annotation["bbox"]
-        
+
     def get_bbox_polygon(self, image_path):
         """
         https://github.com/cocodataset/cocoapi/issues/34
         """
 
         bbox = self.get_bbox(image_path)
-        x_min, y_min, width, height = bbox 
+        x_min, y_min, width, height = bbox
         points = [
-            (x_min, y_min), (x_min + width, y_min), (x_min + width, y_min + height), (x_min, y_min + height)
+            (x_min, y_min),
+            (x_min + width, y_min),
+            (x_min + width, y_min + height),
+            (x_min, y_min + height)
         ]
         return Polygon(points)
-
 
     def get_segmentation(self, image_path):
         """
@@ -49,7 +51,7 @@ class CocoAnnotationsManager(AnnotationsManager):
 
         annotation = self.get_annotation(image_path=image_path)
         return annotation["segmentation"][0]
-        
+
     def get_segmentation_polygon(self, image_path):
         segmentation = self.get_segmentation(image_path)
         points = []
